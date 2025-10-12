41d83dbbaee14407bb6c8579569493143ca6213a
aylilin <aylinshahrokhi85@gmail.com>
2025-10-12 23:24:17 +0330

    startgit add miniProject.cpp!

---
+++ miniProject.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Student{
+    string name;
+    int studentID;
+    int units;
+    float gpa;
+    string securityCode;
+    Student* next;
+};
+
+void addStudent(Student* &head, const string& name, int studentID, int units, float gpa, const string& securityCode)
+{
+    Student* newStudent = new Student;
+
+    newStudent -> name = name;
+    newStudent -> studentID = studentID;
+    newStudent -> units = units;
+    newStudent -> gpa = gpa;
+    newStudent -> securityCode = securityCode;
+    newStudent -> next = nullptr;
+
+    //if list is empty
+    if (head == nullptr){
+        head = newStudent;
+        return;
+    }
+
+    Student* current = head;
+}
+
+bool deleteStudent(Student* &head, int studentID)
+{
+    if (head == nullptr){
+        return false;
+    }
+
+    Student* current = head;
+    Student* prev = nullptr;
+
+    while (current != nullptr && current->studentID != studentID){
+        prev = current;
+        current = current->next;
+    }
+
+    //if student not found
+    
+}
